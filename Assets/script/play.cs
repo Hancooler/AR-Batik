91465c48b50283ceafdaa8c3a5aa81cdc2df717e
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-06-30 09:39:23 +0700

    Initial commit ltfs
    backup with ltfs

---
+++ Assets/script/play.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class play : MonoBehaviour
+{
+    public void batikunsil()
+    {
+        SceneManager.LoadScene("batikunsil");
+      
+    }
+
+    public void batikgarutan()
+    {
+        SceneManager.LoadScene("batikgarutan");
+    }
+
+}
