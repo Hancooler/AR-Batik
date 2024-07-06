a473441f07534d54edb64d6f5561a8357da11f6d
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-07-06 18:09:27 +0700

    code

--- Assets/script/play.cs
+++ Assets/script/play.cs
@@ -14,6 +14,41 @@ public class play : MonoBehaviour
     public void batikgarutan()
     {
         SceneManager.LoadScene("batikgarutan");
+
+    }
+    public void batikParangsolo()
+    {
+        SceneManager.LoadScene("batikparangsolo");
+    }
+
+    public void batiklampung()
+    {
+        SceneManager.LoadScene("batiklampung");
+    }
+
+    public void batikkaltim()
+    {
+        SceneManager.LoadScene("batikkalimantantimur");
+    }
+
+    public void batikmadura()
+    {
+        SceneManager.LoadScene("batikmadura");
+    }
+
+    public void batikbetawi()
+    {
+        SceneManager.LoadScene("batikbetawi");
+    }
+
+    public void batikpapua()
+    {
+        SceneManager.LoadScene("batikpapua");
+    }
+
+    public void batikpringsedapurmagetan()
+    {
+        SceneManager.LoadScene("batikpringsedapurmagetan");
     }
 
 }
