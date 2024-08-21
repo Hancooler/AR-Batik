1f23fb019beb5a55f5da7955a368251d7d4f7ffe
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-08-21 09:24:13 +0700

    add loading
    test

--- Assets/Sceneloading.cs
+++ Assets/Sceneloading.cs
@@ -6,25 +6,12 @@ using UnityEngine.UI;
 public class SceneLoader : MonoBehaviour
 {
     public GameObject LoaderUI;
-    public Image Loadingbarfill;
+  
 
     public void LoadScene(int index)
     {
         StartCoroutine(LoadScene_Coroutine(index));
     }
 
-    IEnumerator LoadScene_Coroutine(int index)
-    {
-        AsyncOperation Operation = SceneManager.LoadSceneAsync(index);
-      
-        LoaderUI.SetActive(true);
-    
-        while (!Operation.isDone)
-        {
-            float progressValue = Mathf.Clamp01(Operation.progress / 0.9f);
-
-            Loadingbarfill.fillAmount = progressValue;
-            yield return null;
-        }
-    }
+   
 }
