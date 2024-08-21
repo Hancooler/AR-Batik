244d73121be62c232236efc35b820ad8696e8af1
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-08-21 10:25:01 +0700

    fixed bug

--- Assets/script/play.cs
+++ Assets/script/play.cs
@@ -3,105 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class play : MonoBehaviour
+public class Play : MonoBehaviour
 {
-    public void batikunsil()
+    public void buttonplay()
     {
-        SceneManager.LoadScene("batikunsil");
-      
-    }
-
-    public void batikgarutan()
-    {
-        SceneManager.LoadScene("batikgarutan");
-
-    }
-    public void batikParangsolo()
-    {
-        SceneManager.LoadScene("batikparangsolo");
-    }
-
-    public void batiklampung()
-    {
-        SceneManager.LoadScene("batiklampung");
-    }
-
-    public void batikkaltim()
-    {
-        SceneManager.LoadScene("batikkalimantantimur");
-    }
-
-    public void batikmadura()
-    {
-        SceneManager.LoadScene("batikmadura");
-    }
-
-    public void batikbetawi()
-    {
-        SceneManager.LoadScene("batikbetawi");
-    }
-
-    public void batikpapua()
-    {
-        SceneManager.LoadScene("batikpapua");
-    }
-
-    public void batikpringsedapurmagetan()
-    {
-        SceneManager.LoadScene("batikpringsedapurmagetan");
-    }
-
-    public void batiksolosidomukti()
-    {
-        SceneManager.LoadScene("batiksidomukti");
-    }
-
-    public void batikmegamendung()
-    {
-        SceneManager.LoadScene("batikmegamendung");
-    }
-
-    public void batiktubo()
-    {
-        SceneManager.LoadScene("batiktubo");
-    }
-
-    public void batiktambalyogyakarta()
-    {
-        SceneManager.LoadScene("batiktambal");
-    }
-
-    public void batiksingabarong()
-    {
-        SceneManager.LoadScene("batiksingabarong");
-    }
-
-    public void batiksidoluhur()
-    {
-        SceneManager.LoadScene("batiksidoluhur");
-    }
-
-    public void batiksekarjagad()
-    {
-        SceneManager.LoadScene("batiksekarjagad");
-    }
-    public void batikpriangan()
-    {
-        SceneManager.LoadScene("batikpriangan");
-    }
-
-    public void batiklasemmerahrembang()
-    {
-        SceneManager.LoadScene("batiklasemmerahrembang");
-    }
-
-    public void batikkawung()
-    {
-        SceneManager.LoadScene("batikkawung");
-    }
-
-    public void batik7rupa()
-    {
-        SceneManager.LoadScene("batik7rupa");
+        SceneManager.LoadScene("Play");
     }
 }
