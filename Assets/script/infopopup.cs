7394af7c127b60b581929482fe44b6d9819176c4
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-08-24 22:22:53 +0700

    add loading scene

---
+++ Assets/script/infopopup.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class infopopup : MonoBehaviour
+{
+    public void batik7rupa()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batikgarutan()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batikbetawi()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batikkalimantan()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batikkawung()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batiklampung()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batiklasemmerahrembang()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batikmadura()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batikmegamendung()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batikpapua()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batikparang()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batikpriangan()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batikpringsedapurmagetan()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batiksekarjagad()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batiksidoluhur()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batiksidomukti()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batiksingabarong()
+    {
+        SceneManager.LoadScene("loadinginfo");
+
+    }
+    public void batiktambal()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    public void batiktubo()
+    {
+        SceneManager.LoadScene("loadinginfo"); 
+
+    }
+    public void batikunsil()
+    {
+        SceneManager.LoadScene("loadinginfo");
+    }
+    
+}
