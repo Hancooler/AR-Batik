ddcb4b940dc1fab5b98411a918368fe792b09f5d
Hancooler <76528360+Hancooler@users.noreply.github.com>
2024-08-26 21:53:58 +0700

    fixed isue

--- Assets/loadingBar/scripts/rotatetotate.cs
+++ Assets/loadingBar/scripts/rotatetotate.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class rotatetotate : MonoBehaviour {
-
+public class RotatingImage : MonoBehaviour
+{
     private RectTransform rectComponent;
     private Image imageComp;
-    private bool up = false;
 
+    [Header("Rotation Settings")]
     public float rotateSpeed = 200f;
+    public Vector3 rotationAxis = Vector3.forward; // Default rotation axis is the z-axis
+    public bool clockwise = true; // Default rotation direction is clockwise
+    public float minRotation = 0f;
+    public float maxRotation = 360f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rectComponent = GetComponent<RectTransform>();
         imageComp = rectComponent.GetComponent<Image>();
-
     }
-	
-	// Update is called once per frame
-	void Update () {
 
+    // Update is called once per frame
+    void Update()
+    {
         float currentSpeed = rotateSpeed * Time.deltaTime;
-        rectComponent.Rotate(0f, 0f, currentSpeed);
+        float rotationAngle = clockwise ? currentSpeed : -currentSpeed;
+
+        // Apply rotation, ensuring it stays within the specified limits
+        rectComponent.Rotate(rotationAxis, rotationAngle);
+        float currentRotation = rectComponent.eulerAngles.z; // Assuming z-axis is the rotation axis
+        currentRotation = Mathf.Clamp(currentRotation, minRotation, maxRotation);
+        rectComponent.eulerAngles = new Vector3(0f, 0f, currentRotation);
     }
 }
